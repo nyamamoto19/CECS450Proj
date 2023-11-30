5ef1fccedb4cbff9a600eca6b6ec6686c0114c44
Nicholas Yamamoto <54457924+nyamamoto19@users.noreply.github.com>
2023-11-30 11:57:38 -0800

    Updated graphs

--- n.r
+++ n.r
@@ -1,8 +1,13 @@
 install.packages("sqldf")
 install.packages("readxl")
+install.packages("ggthemes")
+install.packages("extrafont")
 library("readxl")
 library("sqldf")
 library("ggplot2")
+library("ggthemes")
+library("extrafont")
+
 prices = read.csv("/Users/yamahon/Desktop/CECS450Proj/archive/car_prices.csv")
 energy = read.csv("/Users/yamahon/Desktop/CECS450Proj/archive/global-energy-substitution.csv")
 sql <- "SELECT year,AVG(price) as price
@@ -12,7 +17,12 @@ result <- sqldf(sql)
 print(result)
 
 graph <-ggplot(result,aes(x = year,y=price))
-graph <- graph + geom_line(stat='identity')+xlim(2010,2014) +ylim(20000,35000) + geom_point(stat='identity') +ylim(20000,35000)
+graph <- graph + geom_line(stat='identity')+xlim(2010,2014) +ylim(20000,35000) + geom_point(stat='identity') +ylim(20000,35000)+
+labs(y = "Price",
+     x = "Year",
+     title = "Average Gas Car Cost per Year")+
+  theme_wsj()+
+  theme(axis.title= element_text(), text = element_text(family="Trebuchet MS"))
 print(graph)
 
 
@@ -23,12 +33,26 @@ sql <- "SELECT year,quantity
 result <- sqldf(sql)  
 print(result)
 graph <-ggplot(result,aes(x = year,y=quantity))
-graph <- graph + geom_line(stat='identity') + geom_point(stat='identity') +xlim(2010,2014)
+graph <- graph + geom_line(stat='identity') + geom_point(stat='identity') +
+  xlim(2010,2014)+
+  labs(y = "Quantity (Metric Tons-thousands)",
+       x = "Year",
+       title = "Gas Usage per Year in America",
+       subtitle = "Specifically used for transportation")+
+  theme_wsj()+
+  theme(axis.title= element_text(), text = element_text(family="Trebuchet MS"))
 print(graph)
 
 elec = read.csv("/Users/yamahon/Desktop/CECS450Proj/archive/elecvehicle.csv")
 
-elecgraph <- ggplot(elec,aes(x=Year,y=averageprice)) +geom_line(stat = 'identity') + geom_point(stat='identity')
+elecgraph <- ggplot(elec,aes(x=Year,y=averageprice)) +
+  geom_line(stat = 'identity') + 
+  geom_point(stat='identity') + 
+  labs(y = "Average Car Price",
+       x = "Year",
+       title = "Average Electric Car Price per Year")+
+  theme_wsj()+
+  theme(axis.title= element_text(), text = element_text(family="Trebuchet MS"))
 print(elecgraph)
 
 sql <- "SELECT year,quantity
@@ -37,5 +61,26 @@ sql <- "SELECT year,quantity
 result <- sqldf(sql)  
 print(result)
 graph <-ggplot(result,aes(x = year,y=quantity))
-graph <- graph + geom_line(stat='identity') + geom_point(stat='identity')
+graph <- graph + geom_line(stat='identity') + 
+  geom_point(stat='identity') + 
+  labs(y = "Quantity (Kilowatts-thousands)",
+       x = "Year",
+       title = "Electric Usage per Year in America")+
+  theme_wsj()+
+  theme(axis.title= element_text(), text = element_text(family="Trebuchet MS"))
 print(graph)
+fonts()
+
+#Both car prices
+sql <- "SELECT year as Year,AVG(price) as averageprice
+        FROM prices
+        GROUP BY year"
+result <- sqldf(sql)  
+
+graph <-  ggplot(elec,aes(x=Year,y=averageprice)) +
+  geom_line(data = result) + 
+  geom_line(data = elec) + 
+  xlim(2010,2018)
+
+print(graph)
+
